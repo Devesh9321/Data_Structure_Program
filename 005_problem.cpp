a491bd4d64f2e716f9d899cd62e4843eecc3c7c6
“Devesh9321” <deveshpatilamt@gmail.com>
2024-04-26 22:27:56 +0530

    done

--- 005_problem.cpp
+++ 005_problem.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 /* * * * Longest Common Sequence * * * */
-main()
+string findlcs(const string X, const string Y)
 {
-    string X = "CGATAATTGAGA", Y = "GTTCCTAATA", result = "";
     int sizex = X.size() + 1, sizey = Y.size() + 1;
-
     int dp[sizex][sizey];
 
     for (int i = 0; i < sizex; i++)
@@ -32,34 +30,40 @@ main()
     }
 
     // print the dp array
-    for (int i = 0; i < sizex; i++)
+    // for (int i = 0; i < sizex; i++)
+    // {
+    //     for (int j = 0; j < sizey; j++)
+    //     {
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << "Longest String will be : " << dp[sizex - 1][sizey - 1] << endl;
+
+    // Using the backtracking to get the string.
+    int i = sizex - 1, j = sizey - 1;
+    string lcs;
+    while (i > 0 && j > 0)
     {
-        for (int j = 0; j < sizey; j++)
+        if (X[i - 1] == Y[j - 1])
         {
-            cout << dp[i][j] << " ";
+            lcs = X[i - 1] + lcs;
+            i--;
+            j--;
         }
-        cout << endl;
+        else if (dp[i - 1][j] > dp[i][j - 1])
+            i--;
+        else
+            j--;
     }
 
-    cout << "Longest String will be : " << dp[sizex - 1][sizey - 1] << endl;
+    return lcs;
+}
 
-    for (int i = sizex - 1; i >= 0; i--)
-    {
-        for (int j = sizey - 1; j >= 0; j--)
-        {
-            if (dp[i][j] > dp[i][j - 1])
-            {
-                // cout << dp[i][j] << " " << i << " " << j << " " << X[i - 1] << " " << Y[j - 1] << endl;
-                result.insert(0, 1, Y[i - 1]);
-                if (j == 1)
-                {
-                    goto finalans;
-                }
-                j = -1;
-            }
-        }
-    }
-finalans:
-    cout << "The Longest string will be : " << result;
+main()
+{
+    string X = "CGATAATTGAGA", Y = "GTTCCTAATA";
+    string lcs = findlcs(X, Y);
+    cout << "The Longest string will be : " << lcs;
     return 0;
 }
