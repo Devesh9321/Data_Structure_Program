9214cf56c32b1e3ba47cb0971b7cb6f3bb951eae
Devesh Patil <deveshpatilamt@gmail.com>
2024-10-11 23:11:27 +0530

    Update CF_0007.py

--- CodeForces/CF_0007.py
+++ CodeForces/CF_0007.py
@@ -1,47 +1,36 @@
 
 # Problem Link
 # https://codeforces.com/contest/2009/problem/F
-from collections import deque
 
-def Solution(n,q,arr):
-    # using Maths
-    ps = [0]
-    for e in arr:
-        ps.append( ps[-1] + e )
-    for e in arr:
-        ps.append( ps[-1] + e )
+
+def Solution(n,k,q,arr):
+    answer = [0]*n
+    for i in range(n):
+        arr[i] -= i
+    print(arr)
     
     for _ in range(q):
             l,r = map(int,input().split(' '))
-            l,r = l-1,r-1
-            i,j = l//n,r//n
-            l,r = l%n,r%n
-            print( ps[n] * (j-i+1) - (ps[i+l] - ps[i]) - ( ps[j+n] - ps[j+r+1]) )
-
-    """
-    # Using Brutforce
-    que = deque(arr)
-    b = []
-    for _ in range((l%n)+(r%n)):
-        b.extend(que)
-        que.rotate(-1)
-    print( sum(b[l-1:r]) ) 
-    """
+            # print(  findlongestConstSequence(arr[l-1:r]))
 
 if __name__=="__main__":
     t = int(input())
     for _ in range(t):
-        n,q = map(int,input().split(' '))
+        n,k,q = map(int,input().split(' '))
         arr = list( map(int, input().split(' ') ) )
-        Solution(n,q,arr)
+        Solution(n,k,q,arr)
 
 """
 Input
 1
-3 1
-1 2 3
-1 9
+7 5 3
+1 2 3 2 1 2 3
+1 5
+2 6
+3 7
 
 Output
-18
+2
+3
+2
 """
